22d92813cb4202811fced0bc0cf1582cbc528629
bkim9 <bek001@ucsd.edu>
2022-10-20 11:16:24 -0700

    not typo sorry

--- expose/javascript/part1-question3.js
+++ expose/javascript/part1-question3.js
@@ -1,13 +1,8 @@
 function sumValues(num1, num2, add) {
-    
     if(add) {
-
         let result = 0;
-
         result = num1 + num2;
-
-        console.log('values add: ', result);
-
+        console.log('values added: ', result);
     } else return;
 
     console.log('final result: ', result);
--- expose/javascript/part1-question5.js
+++ expose/javascript/part1-question5.js
@@ -1,8 +1,13 @@
 function sumValues(num1, num2, add) {
+    
     if(add) {
-        let result = 0;
+
+        const result = 0;
+
         result = num1 + num2;
-        console.log('values added: ', result);
+
+        console.log('values add: ', result);
+
     } else return;
 
     console.log('final result: ', result);
